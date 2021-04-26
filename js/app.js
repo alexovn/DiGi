fa1ded4e0945f9051f54c24a755d928fe309b32a
Nikita Alexov <64487090+alexovn@users.noreply.github.com>
2021-04-26 12:24:01 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -44,6 +44,87 @@ $(function() {
       });
 })
 
-if('ontouchstart' in window) {
-    document.addEventListener('touchstart', function(e){}, true);
+function touchstart(){
+    if('ontouchstart' in window) {
+        document.addEventListener('touchstart', function(e){}, true);
+    }
+}
+
+const navToggle = document.getElementById('nav-toggle');
+const sidebarMenu = document.getElementById('sidebar-menu');
+const wrapper = document.querySelector('.wrapper');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+
+const timeout = 450;
+
+
+function menuActivation() {
+	navToggle.classList.toggle('active');
+    sidebarMenu.classList.toggle('active');
+    wrapper.classList.toggle('active');
+
+    if (sidebarMenu.classList.contains('active')) {
+        bodyLock();
+    } else {
+        bodyUnLock();
+    }
+}
+
+function menuDeactivation() {
+	navToggle.classList.remove('active');
+    sidebarMenu.classList.remove('active');
+    wrapper.classList.remove('active');
+	bodyUnLock();
+}
+
+if (navToggle) {
+    navToggle.addEventListener('click', function(e) {
+        menuActivation();
+    })
+}
+
+document.addEventListener('click', function(e) {
+	if(!e.target.matches('.nav-toggle, .nav-toggle *, .sidebar-menu, .sidebar-menu *')) {
+		menuDeactivation();
+	}
+})
+
+
+function bodyLock() {
+	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+	if (lockPadding.length > 0) {
+		for (let index = 0; index < lockPadding.length; index++) {
+			const el = lockPadding[index];
+			el.style.paddingRight = lockPaddingValue;
+		}
+	}
+	body.style.paddingRight = lockPaddingValue;
+	body.classList.add('lock');
+
+    // unlock = false;
+	setTimeout(function () {
+		unlock = true;
+	}, timeout);
+}
+
+function bodyUnLock() {
+	setTimeout(function () {
+		if (lockPadding.length > 0) {
+			for (let index = 0; index < lockPadding.length; index++) {
+				const el = lockPadding[index];
+				el.style.paddingRight = '0px';
+			}
+		}
+		body.style.paddingRight = '0px';
+		body.classList.remove('lock');
+	}, timeout);
+
+    // unlock = false;
+	setTimeout(function () {
+		unlock = true;
+	}, timeout);
 }
